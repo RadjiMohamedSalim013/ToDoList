0e970615d87e288a674f0028827496f52679adde
Radji Mohamed <radjimohamed013@gmail.com>
2024-08-22 21:46:34 +0100

    ajout

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ load_dotenv()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
